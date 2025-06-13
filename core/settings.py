54cacd8495ef3c9a7af199bfaf7640e87ceb8259
Giorgio Morello <morellogiorgio327@gmail.com>
2025-06-13 17:23:08 -0300

    config

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG')
-
+#
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda x: [routes for routes in x.split(', ')])
 
 
