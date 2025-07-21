52b2a74c442a9163ff9c6d4a33d8d8ec7151f62d
Giorgio Morello <morellogiorgio327@gmail.com>
2025-07-21 11:27:31 -0300

    fix DEBUG variable

--- core/__init__.py
+++
@@ -1,4 +0,0 @@
-from .celery_django import app as celery_app
-
-
-__all__ = ['celery_app', ]
--- user/utils/otp_token.py
+++
@@ -1,4 +0,0 @@
-from secrets import token_hex
-
-def generate_otp(nbytes):
-    return token_hex(nbytes)
