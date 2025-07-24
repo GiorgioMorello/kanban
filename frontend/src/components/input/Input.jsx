ce1edbeb1ed8533ebf2e951f33ddfe928b8695c5
Giorgio Morello <morellogiorgio327@gmail.com>
2025-07-24 11:27:13 -0300

    add forwardRef in Input component

--- frontend/src/components/input/Input.jsx
+++ frontend/src/components/input/Input.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 import {NavLink, Link} from 'react-router-dom'
 import styles from './Input.module.css'
 
 
-export default function Input({type, placeholder, name, label_text, required=false, handle_on_change, value, input_ref}){
+function Input({type, placeholder, name, label_text, required=false, handle_on_change, value, data_testid}, ref){
 
 
 
     return (
-
-
             <label className={styles.input_container} htmlFor={name}>
                 <span>{label_text}</span>
-                <input ref={input_ref} value={value} type={type} placeholder={placeholder} id={name} name={name} required={required} onChange={handle_on_change}/>
+                <input data-testid={data_testid} ref={ref} value={value} type={type} placeholder={placeholder} id={name} name={name} required={required} onChange={handle_on_change}/>
             </label>
-
-
 
     )
 }
+
+export default forwardRef(Input)
