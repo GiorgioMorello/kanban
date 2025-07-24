33b36bb8b0d95787a4be33935d9957a7a4a98477
Giorgio Morello <morellogiorgio327@gmail.com>
2025-07-24 11:17:13 -0300

    add try/catch in useRefreshToken.jsx

--- frontend/src/hooks/useRefreshToken.jsx
+++ frontend/src/hooks/useRefreshToken.jsx
@@ -9,14 +9,20 @@ export default function useRefreshToken(){
 
 
     async function refresh(){
-        const resp = await axios_instance.post('user/token/refresh/', {
 
-        });
-        setAccessToken(resp.data.access);
-        //console.log(resp.data.access)
-        setCsrfToken(resp.headers["x-csrftoken"]);
+        try {
+            const resp = await axios_instance.post('user/token/refresh/', {});
 
-        return {access_token: resp.data.access, csrftoken: resp.headers['x-csrftoken']}
+            setAccessToken(resp.data.access);
+            setCsrfToken(resp.headers["x-csrftoken"]);
+
+            return {access_token: resp.data.access, csrftoken: resp.headers['x-csrftoken']}
+
+        } catch (e) {
+            console.error("Erro ao renovar token:", error?.response?.data);
+            return { access_token: null, csrftoken: null };
+        }
+
     }
 
 
